60c8b939a5e1f2c3a6dfc78fb40250f43ddd3d13
Monika Verma <monikakasanya@gmail.com>
2023-09-12 18:11:04 +0530

    remove unused things, added seeds entry also added readme file for installation

--- README.md
+++ README.md
@@ -6,19 +6,21 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
+  ruby-2.6.3 
 
-* System dependencies
+* Installation 
 
-* Configuration
+  git clone "the project"
+
+  run bundle install 
 
 * Database creation
 
-* Database initialization
+  rake db:create  # for create database
+  rake db:migrate # for migrate database
+
+  rake db:seed # for seed the data
 
 * How to run the test suite
 
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+  rspec # for run the test cases
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# create tree from here
+@tree = Tree.first_or_create
+
+#create main node root as per the assignment
+@node = Node.create(label: "root", tree_id: @tree.id)
+
+# create nested nodes with parent node root
+@node_ant = Node.create(label: "ant", tree_id: @tree.id, parent_id: @node.id)
+@node_bear = Node.create(label: "bear", tree_id: @tree.id, parent_id: @node.id)
+@node_frog = Node.create(label: "frog", tree_id: @tree.id, parent_id: @node.id)
